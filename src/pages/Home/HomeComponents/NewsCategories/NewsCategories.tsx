34efaa856c7e3e63b77ffb7ac00f61a23807cc7b
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-02-16 23:23:34 +0200

    Updated design

--- src/pages/Home/HomeComponents/NewsCategories/NewsCategories.tsx
+++ src/pages/Home/HomeComponents/NewsCategories/NewsCategories.tsx
@@ -2,7 +2,7 @@ import "./NewsCategories.scss";
 
 export const NewsCategories: React.FC = () => (
 	<div className="news-categories">
-		{["Нові", "Популярні", "Рекомендовані"].map((category) => (
+		{["Стрічка", "Акції", "Напарники"].map((category) => (
 			<button key={category} className="btn-category">
 				<h2>{category}</h2>
 			</button>
