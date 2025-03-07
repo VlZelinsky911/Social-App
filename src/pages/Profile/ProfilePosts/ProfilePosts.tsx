9400d590c91d9b960e3e5391b405b0d30414ec92
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-03-07 23:16:34 +0200

    Added users profile

---
+++ src/pages/Profile/ProfilePosts/ProfilePosts.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./ProfilePosts.scss";
+import LikeButton from "../../Home/Feed/FeedComponents/InteractionButtons/LikeButton/LikeButton";
+import ShareButton from "../../Home/Feed/FeedComponents/InteractionButtons/SendButton/ShareButton";
+import CommentButton from "../../Home/Feed/FeedComponents/InteractionButtons/CommentButton/CommentButtton";
+interface ProfilePostProps {
+  post: {
+    id: string;
+    text: string;
+    mediaurls?: string;
+    created_at: string;
+    userId: string;
+  };
+}
+
+const ProfilePosts: React.FC<ProfilePostProps> = ({ post }) => {
+  return (
+    <div className="profile-post">
+      <div className="post-header">
+        <div className="post-info">
+          <div className="post-date">
+            {new Date(post.created_at).toLocaleString("uk-UA", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="post-content">
+        <div className="post-content__border">
+          <p className="post-text">{post.text}</p>
+        </div>
+        {post.mediaurls && (
+          <div className="post-media">
+            <img
+              src={post.mediaurls}
+              alt="Post content"
+              className="post-image"
+            />
+          </div>
+        )}
+        <div className="home-likes-comments">
+          <LikeButton contentId={post.id} userId={post.userId} type={"post"} />
+          <CommentButton contentId={post.id} userId={post.userId} />
+          <ShareButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePosts;
